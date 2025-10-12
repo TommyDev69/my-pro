02459207d813225a7ecf2691db9d0ac55d97b2c9
TommyDev69 <sunnevian4life@gmail.com>
2025-10-12 01:54:34 +0100

    update

--- src/Nav/NavContent..jsx
+++ src/Nav/NavContent..jsx
@@ -56,20 +56,61 @@ const NavContent = ({ onMenuClick, openToggle, hoverToggle, setHover}) => {
                   : "opacity-0 translate-y-2 invisible"
               }`}
             >
-              <a href={link} className="px-4 py-2 hover:bg-[#F28123] capitalize">404 page</a>
-              <a href={link} className="px-4 py-2 hover:bg-[#F28123] capitalize">about</a>
-              <a href={link} className="px-4 py-2 hover:bg-[#F28123] capitalize">cart</a>
-              <a href={link} className="px-4 py-2 hover:bg-[#F28123] capitalize">check out</a>
-              <a href={link} className="px-4 py-2 hover:bg-[#F28123] capitalize">contact</a>
-              <a href={link} className="px-4 py-2 hover:bg-[#F28123] capitalize">news</a>
-              <a href={link} className="px-4 py-2 hover:bg-[#F28123] capitalize rounded-b-xl">shop</a>
+              <a href={link} className="px-4 py-2 hover:bg-[#F28123] capitalize rounded-t-xl">404 page</a>
+              <a href={link} className="px-4 py-2 hover:bg-[#F28123] capitalize rounded-b-xl">about</a>
+              <a href={link} className="px-4 py-2 hover:bg-[#F28123] capitalize rounded-t-xl">cart</a>
+              <a href={link} className="px-4 py-2 hover:bg-[#F28123] capitalize rounded-b-xl">check out</a>
+              <a href={link} className="px-4 py-2 hover:bg-[#F28123] capitalize rounded-t-xl">contact</a>
+              <a href={link} className="px-4 py-2 hover:bg-[#F28123] capitalize rounded-b-xl">news</a>
+              <a href={link} className="px-4 py-2 hover:bg-[#F28123] capitalize rounded-t-xl">shop</a>
             </div>
           </div>
 
           {/* OTHER LINKS */}
+          <div className="relative group" onMouseEnter={() =>setHover('news')}
+             onMouseLeave={() =>setHover(null)}>
           <a href={link} className="capitalize text-2xl transition hover:text-[#F28123]">news</a>
+             <div
+              className={`absolute left-0 top-full mt-2 bg-white text-black px-2 rounded-xl shadow-lg flex flex-col w-56 transition-all duration-300 z-50 ${
+                hoverToggle === "news"
+                  ? "opacity-100 translate-y-1 visible"
+                  : "opacity-0 translate-y-2 invisible"
+              }`}
+            >
+              <a href={link} className="px-4 py-2 hover:bg-[#F28123] rounded-t-xl capitalize">
+                news
+              </a>
+              <a href={link} className="px-4 py-2 hover:bg-[#F28123] rounded-b-xl capitalize">
+                single news
+              </a>
+            </div>
+          </div>
           <a href={link} className="capitalize text-2xl transition hover:text-[#F28123]">contact</a>
+
+          <div className="relative group" onMouseEnter={() =>setHover('shop')}
+             onMouseLeave={() =>setHover(null)}>
           <a href={link} className="capitalize text-2xl transition hover:text-[#F28123]">shop</a>
+           <div
+              className={`absolute left-0 top-full mt-2 bg-white text-black px-2 rounded-xl shadow-lg flex flex-col w-56 transition-all duration-300 z-50 ${
+                hoverToggle === "shop"
+                  ? "opacity-100 translate-y-1 visible"
+                  : "opacity-0 translate-y-2 invisible"
+              }`}
+            >
+              <a href={link} className="px-4 py-2 hover:bg-[#F28123] rounded-t-xl capitalize">
+                shop
+              </a>
+              <a href={link} className="px-4 py-2 hover:bg-[#F28123] rounded-b-xl capitalize">
+                check out
+              </a>
+              <a href={link} className="px-4 py-2 hover:bg-[#F28123] rounded-t-xl capitalize">
+                single product
+              </a>
+              <a href={link} className="px-4 py-2 hover:bg-[#F28123] rounded-b-xl capitalize">
+                cart
+              </a>
+            </div>
+        </div>
         </div>
       </div>
 
