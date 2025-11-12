7505f298fa08db93ca64e99d96ac4c813d804f49
TommyDev69 <sunnevian4life@gmail.com>
2025-11-12 22:51:05 +0100

    update

--- src/Slide/MainSlide.jsx
+++ src/Slide/MainSlide.jsx
@@ -8,7 +8,7 @@ import img3 from "../image/avaters/avatar2.png";
 import SlideDetail from "./SlideDetail";
 const Mainslide = ({ Info }) => {
    const [data,setData] = useState([
-    {id:1, Image:img1, name:'Ajabge Tomiwa', alias:'linux', title:'software develop', content:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam sunt quod beatae corrupti atque velit quos et aperiam, expedita dolorem, repellendus sint. Itaque, architecto! Non ad eius labore cum iusto.'},
+    {id:1, Image:img1, name:'Ajagbe Tomiwa', alias:'linux', title:'software develop', content:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam sunt quod beatae corrupti atque velit quos et aperiam, expedita dolorem, repellendus sint. Itaque, architecto! Non ad eius labore cum iusto.'},
     {id:2, Image:img2, name:'Banwo olamiposi', alias:'atomi', title:'local shop owner', content:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam sunt quod beatae corrupti atque velit quos et aperiam, expedita dolorem, repellendus sint. Itaque, architecto! Non ad eius labore cum iusto.'},
     {id:3, Image:img3, name:'jacob sikim', alias:'jux456', title:'local shop owner', content:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam sunt quod beatae corrupti atque velit quos et aperiam, expedita dolorem, repellendus sint. Itaque, architecto! Non ad eius labore cum iusto.'},
     {id:4, Image:img4, name:'saira hakim', alias:'saya321', title:'local distributor', content:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam sunt quod beatae corrupti atque velit quos et aperiam, expedita dolorem, repellendus sint. Itaque, architecto! Non ad eius labore cum iusto.'},
