af9c0dfdb1bc89e185eb843129faab1077b9bc3d
TommyDev69 <sunnevian4life@gmail.com>
2025-11-13 20:03:11 +0100

    update

--- src/promo section/PromoInfo.jsx
+++ src/promo section/PromoInfo.jsx
@@ -2,7 +2,7 @@ import DisplayTime from "./DisplayTime";
 
 const PromoInfo = ({title, content,TimeLeft}) => {
     return ( 
-        <div className="md:w-[50%] md:px-16 py-6">
+        <div className="md:w-[60%] md:px-16 py-6">
 
         <div className="flex  justiwfy-center  pt-16 items-center text-[15px] md:text-[28px] tex w-full  font-bold ">
             <p className=" capitalize text-[rgb(242,129,35)]">deal</p>
