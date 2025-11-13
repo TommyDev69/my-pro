1c3170dccea05ebe490015bc9d20fdf683a8a993
TommyDev69 <sunnevian4life@gmail.com>
2025-11-13 21:23:59 +0100

    update

--- src/promo section/PromoInfo.jsx
+++ src/promo section/PromoInfo.jsx
@@ -8,7 +8,7 @@ const PromoInfo = ({title, content,TimeLeft}) => {
             <p className=" capitalize text-[rgb(242,129,35)]">deal</p>
             <p className="text pl-4">of the month</p>
         </div>
-       <div className="flex flex-col justify-center w-full">
+       <div className="flex flex-col justify-center w-11/12">
          <p className="md:text-[34px] text-[24px] font-extrabold text-[rgb(242,129,35)] md:font-bitcount uppercase">{title}</p>
         <p className="md:text-[16px] text-[7px]  pb-14">{content}</p>
        </div>
