bf3dc24d61c3f98ce89c2a69a7c905a44c2e10df
TommyDev69 <sunnevian4life@gmail.com>
2025-11-13 11:58:47 +0100

    update

--- src/Nav/NavBrand.jsx
+++ src/Nav/NavBrand.jsx
@@ -3,7 +3,7 @@ import brand_logo from '../image/brand logo.png'
 export default function NavBrand() {
   return (
     <div className=' py-7 md:py-7 w-20 '>
-      <img src={brand_logo} alt='logo' className='md:w-40 w-20'/>
+      <img src={brand_logo} alt='logo' className='md:w-40 w-16'/>
     </div>
   )
 }
