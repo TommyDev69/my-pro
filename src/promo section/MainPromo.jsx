9bc43629d31eff5a101a23e9a6da273e9ab74f78
TommyDev69 <sunnevian4life@gmail.com>
2025-11-13 20:50:10 +0100

    update

--- src/promo section/MainPromo.jsx
+++ src/promo section/MainPromo.jsx
@@ -56,7 +56,7 @@ const MainPromo = () => {
                   </div>
                 </div>
 
-                <img src={image} alt="fruit" className=' md:w-[38%]' />
+                <img src={image} alt="fruit" className=' md:w-[35%]' />
             </div>
          <PromoInfo title={Ceo} content={content}  TimeLeft = {TimeLeft}  />
         </div>
