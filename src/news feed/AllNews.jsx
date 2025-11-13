cef3085d25f96376ac0a9cfc2c997be09b31f6a9
TommyDev69 <sunnevian4life@gmail.com>
2025-11-13 12:32:47 +0100

    update

--- src/news feed/AllNews.jsx
+++ src/news feed/AllNews.jsx
@@ -10,11 +10,11 @@ export default function AllNews({AllData, icon_1, icon_2}) {
                 {AllData.map((blog)=>(
                     <div className="flex-grow-0 w-full  md:w-full mxc-auto md:mx-0  hover:border-none hover:shadow-[rgb(242,129,35)] shadow-[black] shadow-xl border-2 rounded-3xl" key={blog.id}>
                         <img src={blog.image} alt={blog.news} className="w-full" />
-                        <div className="md:px-8  px-4 py-8">
-                            <p className="md:text-[18px] text-12 text-worksans font-bold text-[#051922cc]">{blog.topic}</p>
-                            <div className="flex my-6 w-full items-center text-pretty justify-around text-slate-500">
-                                <p className="md:text-[14px] text-[8px]"><FontAwesomeIcon icon={icon_1} className='text-[16px]' />&nbsp; Admin</p>
-                                <p className="md:text-[14px] text-[8px] md:pl-20"><FontAwesomeIcon icon={icon_2} className='text-[16px]'/>&nbsp; 10 October 2025</p>
+                        <div className="md:px-4 px-4 py-8">
+                            <p className="md:text-[14px] text-12 text-worksans font-bold text-[#051922cc]">{blog.topic}</p>
+                            <div className="flex my-6 w-full items-center text-pretty justify-aronund text-slate-500">
+                                <p className="md:text-[14px] text-[8px]"><FontAwesomeIcon icon={icon_1} className='text-[14px]' />&nbsp; Admin</p>
+                                <p className="md:text-[14px] text-[8px] md:pl-10 "><FontAwesomeIcon icon={icon_2} className='text-[14px]'/>&nbsp; 10 October 2025</p>
 
                             </div>
 
