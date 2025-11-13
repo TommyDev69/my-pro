1a0c8408414814d0833993ee7b69a472985065d0
TommyDev69 <sunnevian4life@gmail.com>
2025-11-13 16:23:57 +0100

    update

--- src/contact/ContactDetails.jsx
+++ src/contact/ContactDetails.jsx
@@ -7,18 +7,18 @@ import HeroBg from '../image/hero-bg-3.jpg'
 export default function ContactDetails() {
   return (
     <div
-      className="bg-cover  rounded-3xl md:w-3/12 my-14  pb-10 bg-center h-[60vh] flex flex-col text-black justifey-center pl-6"
+      className="bg-cover  rounded-3xl md:w-4/12 my-14  pb-10 bg-center h-[60vh] flex flex-col text-black justifey-center pl-6"
       style={{ backgroundImage: `url(${HeroBg})` }} 
     >
-      <div className="flex items-center pt-6  space-x-3  text-[24px]  rounded-xl">
+      <div className="flex items-center pt-6  space-x-3  text-[20px]  rounded-xl">
         <FontAwesomeIcon icon={faMap} className="text-[#F28123] " />
         <p className="capitalize font-bold">
           Shop Address 
         </p>
       </div>
-       <address className="not-italic px-14 text-[18px] text-[#5a5858] w-8/12">123 Fruit Lane, Lagos, Nigeria.</address>
+       <address className="not-italic px-14 text-[13px] text-[#5a5858] w-8/12">123 Fruit Lane, Lagos, Nigeria.</address>
 
-        <div className="flex items-center pt-6  space-x-3  text-[24px]  rounded-xl">
+        <div className="flex items-center pt-6  space-x-3  text-[20px]  rounded-xl">
         <FontAwesomeIcon icon={faClock} className="text-[#F28123] " />
         <p className="capitalize font-bold">
           Shop hours
@@ -26,16 +26,16 @@ export default function ContactDetails() {
       </div>
       <div className="flex ">
 
-       <p className="not-italic pl-14 text-[15px] text-[#5a5858] ">MON - FRIDAY:</p>
+       <p className="not-italic pl-14 text-[13px] text-[#5a5858] ">MON - FRIDAY:</p>
        <p className="not-italic  text-[15px] pl-1 text-[#5a5858] "> 8 to 9 PM</p>
       </div>
       <div className="flex ">
 
-       <p className="not-italic pl-14 text-[15px] text-[#5a5858] ">SAT - SUN: </p>
+       <p className="not-italic pl-14 text-[13px] text-[#5a5858] ">SAT - SUN: </p>
        <p className="not-italic  text-[15px] pl-1 text-[#5a5858] ">10 to 8 PM</p>
       </div>
 
-       <div className="flex items-center pt-6  space-x-3  text-[24px]  rounded-xl">
+       <div className="flex items-center pt-6  space-x-3  text-[20px]  rounded-xl">
         <FontAwesomeIcon icon={faAddressBook} className="text-[#F28123] " />
         <p className="capitalize font-bold">
           contact 
