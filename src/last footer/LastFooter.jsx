1bfc85ca283438a52f4bdb72abbc45b33342daab
TommyDev69 <sunnevian4life@gmail.com>
2025-11-13 15:15:45 +0100

    update

--- src/last footer/LastFooter.jsx
+++ src/last footer/LastFooter.jsx
@@ -5,9 +5,9 @@ export default function LastFooter({Media, Ceo, title}) {
   return (
     <div className='flex text-[8px] md:text-[20px] container py-8 justify-cewnter space-x-4 md:px-0 px-4'>
         <div className="flex md:w-11/12 w-full md:px-24">
-            <p className="text-white text-[4px] md:text-[14px] py-2 pr-2">copyright &copy;{new Date().getFullYear()}-</p>
-            <p className="text-[rgb(242,129,35)] pt-2">{Ceo},</p>
-            <p className="text-white text-[8px] md:text-[14px] py-2 pl-2">{title}</p>
+            <p className="text-white text-[8px] md:text-[10px] py-2 pr-2">copyright &copy;{new Date().getFullYear()}-</p>
+            <p className="text-[rgb(242,129,35)] text-[10px] md:text-[14px] pt-2">{Ceo},</p>
+            <p className="text-white text-[8px] md:text-[10px] py-2 pl-2">{title}</p>
         </div>
 
         {Media.map((social) =>(
