c0f2f017c2a5b95035c3e14ca4ea25de50dae2e3
TommyDev69 <sunnevian4life@gmail.com>
2025-11-14 16:43:22 +0100

    update

--- src/footer/FootBody.jsx
+++ src/footer/FootBody.jsx
@@ -15,7 +15,7 @@ export default function FootBody({About, getInTouch, Page,Subscribe, iconic, ico
        {getInTouch.map((getting_data) =>(
         <div className="md:space-y-12 w-10/12" key={getting_data.id}>
             <p className="md:text-[24px] text-[18px] font-bold text-white capitalize py-12">{getting_data.Topic}</p>
-            <div className='md:text-[14px] text-white w-full'>
+            <div className='md:text-[12px] text-white w-full'>
                 <p>{getting_data.address}</p>
                 <p className="py-2">{getting_data.email}</p>
                 <p className="">{getting_data.contact}</p>
@@ -27,7 +27,7 @@ export default function FootBody({About, getInTouch, Page,Subscribe, iconic, ico
           {Page.map((page_data) =>(
         <div className="md:space-y-12 w-10/12" key={page_data.id}>
             <p className="text-[18px] md:text-[24px] font-bold text-white capitalize py-12">{page_data.Topic}</p>
-            <div className=' md:text-[14px] text-white w-full font-bold '>
+            <div className=' md:text-[12px] text-white w-full font-bold '>
                <div className="flex pb-4">
                  <FontAwesomeIcon  icon = {iconic}  className='text-[rgb(242,129,35)] '/>
                 <p className="capitalize text hover:text-[rgb(242,129,35)] pl-2">home</p>
@@ -56,11 +56,11 @@ export default function FootBody({About, getInTouch, Page,Subscribe, iconic, ico
           {Subscribe.map((subscribe) =>(
         <div className="md:space-y-12  w-10/12" key={subscribe.id}>
             <p className="text-[18px] md:text-[24px] font-bold text-white capitalize py-12">{subscribe.Topic}</p>
-            <p className="md:text-[14px] text-white w-full">{subscribe.content}</p>
+            <p className="md:text-[12px] text-white w-full">{subscribe.content}</p>
             <form className="py-5">
                 <div className="py-4 flex">
                     <input type="email" placeholder='Email' className='md:p-2 focus:p-4 text-[8px]  md:text-[8px] rounded-xl focus:placeholder-[rgb(242,129,35)] focus-within:bg-[#051922cc] focus-within: text-white border-[#051922cc]' />
-                    <button  className='border-2 md:cmx-2 border-slate-500 rounded-xl md:py-4 md:px-4 px-2 md:text-[9px] text-[rgb(242,129,35)]'><FontAwesomeIcon icon={icon_2} /></button>
+                    <button  className='border-2  border-slate-500 rounded-xl md:py-4 md:px-4 px-2 md:text-[9px] text-[rgb(242,129,35)]'><FontAwesomeIcon icon={icon_2} /></button>
                 </div>
             </form>
 
